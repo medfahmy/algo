f067ab8c2c0196fbd5af7058b3a3b67b73f3bcfb
medfahmy <medfahmyxo@gmail.com>
2022-10-17 11:43:50 +0100

    remove Copy trait bound

--- src/chap2.rs
+++ src/chap2.rs
@@ -2,22 +2,10 @@
 
 use std::collections::VecDeque;
 
-pub enum Order {
-    Asc,
-    Desc,
-}
-
-use Order::*;
-
-pub fn insertion_sort<T: Ord>(slice: &mut [T], order: Order) {
+pub fn insertion_sort<T: Ord>(slice: &mut [T]) {
     for i in 2..slice.len() {
         for j in 0..i {
-            let should_swap = match order {
-                Asc => slice[i] < slice[j],
-                Desc => slice[i] > slice[j],
-            };
-
-            if should_swap {
+            if slice[i] < slice[j] {
                 slice.swap(i, j);
             }
         }
@@ -153,17 +141,14 @@ mod tests {
         let mut arr1: [u8; 1] = [1];
         let mut arr: [u8; 6] = [5, 2, 4, 6, 1, 3];
 
-        insertion_sort(&mut arr0, Asc);
+        insertion_sort(&mut arr0);
         assert_eq!(arr0, []);
 
-        insertion_sort(&mut arr1, Asc);
+        insertion_sort(&mut arr1);
         assert_eq!(arr1, [1]);
 
-        insertion_sort(&mut arr, Asc);
+        insertion_sort(&mut arr);
         assert_eq!(arr, [1, 2, 3, 4, 5, 6]);
-
-        insertion_sort(&mut arr, Desc);
-        assert_eq!(arr, [6, 5, 4, 3, 2, 1]);
     }
 
     #[test]
