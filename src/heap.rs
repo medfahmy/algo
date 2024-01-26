02ab57bfbc3d9e80fe5e133a4000e2d0f01aa727
medfahmy <medfahmy99@gmail.com>
2024-01-26 11:56:50 +0100

    heap sort

--- src/heap.rs
+++ src/heap.rs
@@ -15,25 +15,25 @@ fn right(i: usize) -> usize {
     (i << 1) + 2
 }
 
-pub fn max_heapify<T: Ord + Debug>(arr: &mut [T], i: usize) {
+pub fn max_heapify<T: Ord + Debug>(arr: &mut [T], i: usize, heapsize: usize) {
     let left = left(i);
     let right = right(i);
 
     let mut largest = 0;
 
-    if left < arr.len() && arr[left] > arr[i] {
+    if left < heapsize && arr[left] > arr[i] {
         largest = left;
     } else {
         largest = i;
     }
 
-    if right < arr.len() && arr[right] > arr[largest] {
+    if right < heapsize && arr[right] > arr[largest] {
         largest = right;
     }
 
     if largest != i {
         arr.swap(i, largest);
-        max_heapify(arr, largest);
+        max_heapify(arr, largest, heapsize);
     }
 }
 
@@ -41,7 +41,18 @@ pub fn build_max_heap<T: Ord + Debug>(arr: &mut [T]) {
     let n = arr.len() / 2;
 
     for i in (0..n).rev() {
-        max_heapify(arr, i);
+        max_heapify(arr, i, arr.len());
+    }
+}
+
+pub fn heap_sort<T: Ord + Debug>(arr: &mut [T]) {
+    build_max_heap(arr);
+    let mut heapsize = arr.len();
+
+    for i in (1..arr.len()).rev() {
+        arr.swap(0, i);
+        heapsize -= 1;
+        max_heapify(arr, 0, heapsize);
     }
 }
 
@@ -67,9 +78,10 @@ mod tests {
     #[test]
     fn max_heapify_works() {
         let mut arr = [16, 4, 10, 14, 7, 9, 3, 2, 8 , 1];
+        let heapsize = arr.len();
         let expected = [16, 14, 10, 8, 7, 9, 3, 2, 4 , 1];
 
-        max_heapify(&mut arr, 1);
+        max_heapify(&mut arr, 1, heapsize);
 
         assert_eq!(arr, expected);
     }
@@ -82,5 +94,21 @@ mod tests {
         build_max_heap(&mut arr);
 
         assert_eq!(arr, expected);
+    }
+
+    #[test]
+    fn heapsort_works() {
+        let mut arr0: [u8; 0] = [];
+        let mut arr1: [u8; 1] = [1];
+        let mut arr: [u8; 6] = [5, 2, 4, 6, 1, 3];
+
+        heap_sort(&mut arr0);
+        assert_eq!(arr0, []);
+
+        heap_sort(&mut arr1);
+        assert_eq!(arr1, [1]);
+
+        heap_sort(&mut arr);
+        assert_eq!(arr, [1, 2, 3, 4, 5, 6]);
     }
 }
